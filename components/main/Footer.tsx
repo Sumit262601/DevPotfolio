c60ecf217834db88cb73f5e868b98792e9878937
Sumit262601 <skr.04088s@gmail.com>
2024-04-27 18:49:39 +0530

    Footer update

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -40,6 +40,10 @@ const Footer = () => {
                             <FaLinkedin />
                             <span className="text-[15px] ml-[6px]">Linkedin</span>
                         </p>
+                        <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                            <RxDiscordLogo />
+                            <span className="text-[15px] ml-[6px]">Discord</span>
+                        </p>
                     </div>
                     <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
                         <div className="font-bold text-[16px]">About</div>
